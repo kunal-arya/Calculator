2ad250383281296cb9300d03806a793b41fafb2c
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-07-05 13:45:17 +0530

    initial commit

--- javascript.js
+++ javascript.js
@@ -1,15 +1,1 @@
-let userInput = document.querySelector(".result");
-let buttons = document.querySelectorAll(".buttons");
-let allClear = document.querySelector("#allClearBtn");
-
-function clearClickHandler() {
-    return userInput.value = "";
-}
-    
-function buttonsClickHandler(){
-    console.log("clicked");
-}
-
-allClear.addEventListener("click" , clearClickHandler);
-
-buttons.addEventListener("click" , buttonsClickHandler);
+let readlineSync = require("readline-sync");
